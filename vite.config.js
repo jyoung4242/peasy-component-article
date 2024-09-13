25a1adba408dc274e60874459ca9fd79a6e4f5aa
Justin Young <justin_dean_young@yahoo.com>
2024-09-13 14:05:08 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "./",
-};
+  esbuild: {
+    supported: {
+      "top-level-await": true, //browsers can handle top-level-await features
+    },
+  },
+});
